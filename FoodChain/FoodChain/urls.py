672fb11e95cebcd3e93dc4074140fd6673dd5c1b
Samveg12 <68071531+Samveg12@users.noreply.github.com>
2020-10-13 11:39:53 +0530

    Naming sorted

--- FoodChain/FoodChain/urls.py
+++ FoodChain/FoodChain/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
     path('NGO/', include('NGO.urls')),
     path('Donate/',include('Donate.urls'))
 ]
