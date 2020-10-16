a68f75a1a124b7182f0b1318e514c9cf285574d6
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2020-10-16 08:26:25 +0530

    "type your password here"

--- FoodChain/FoodChain/settings.py
+++ FoodChain/FoodChain/settings.py
@@ -130,4 +130,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mihir.nikam1@gmail.com'
-EMAIL_HOST_PASSWORD = "Mihir-sahir_12345"
+EMAIL_HOST_PASSWORD = "type your password here"
