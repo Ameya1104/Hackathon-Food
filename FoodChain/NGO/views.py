ef28768ece7daf4eeee7353a3015e34355d6e641
Rutvik-C <rutvikchoudhari97@gmail.com>
2020-10-16 21:32:29 +0530

    local save

--- FoodChain/NGO/views.py
+++ FoodChain/NGO/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from bootstrap_datepicker_plus import DateTimePickerInput
+# from bootstrap_datepicker_plus import DateTimePickerInput
 from .forms import Registerdetail, Food
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth import authenticate, login, logout
