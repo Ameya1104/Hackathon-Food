32ac10c19896257467178df11ca47a172f08ca8d
Samveg12 <68071531+Samveg12@users.noreply.github.com>
2020-10-16 10:03:26 +0530

    Timezon

--- FoodChain/FoodChain/settings.py
+++ FoodChain/FoodChain/settings.py
@@ -119,8 +119,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
+TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
