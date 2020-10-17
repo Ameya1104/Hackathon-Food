ef8a4bec1ecab045c9ebdd1e088caf098566add0
Samveg12 <68071531+Samveg12@users.noreply.github.com>
2020-10-17 15:27:51 +0530

    History set up

--- FoodChain/Donate/views.py
+++ FoodChain/Donate/views.py
@@ -3,7 +3,7 @@ from django .http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
-from NGO.models import Belongs,foodAvbl,otherDetails,Cities
+from NGO.models import Belongs,foodAvbl,otherDetails,Cities,History
 from NGO.forms import Registerdetail,otherDetails,foodAvbl
 from .forms import FoodRequest,Rate
 from .models import FoodReq,rate, orders
@@ -103,6 +103,8 @@ def loginpage(request):
                     if i.created_on != None:
                         i.created_on -= timedelta(hours=6)
                         i.created_on.update()
+                        history = History(user=i.user,otherDetails=i.otherDetails,measurement=i.measurement,typee=i.typee,quantity=i.quantity,Other_Specifics=i.Other_Specifics,images=i.images,city=i.city,pickup_address=i.pickup_address,created_on=i.created_on,edible=i.edible)
+                        history.save()
                         i.created_on += timedelta(hours=i.edible)
                         print(i.created_on)
                         if now>i.created_on:
